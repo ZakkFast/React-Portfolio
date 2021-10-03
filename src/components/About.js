abdc130cb4208282bd9c7d6c464e5666eacfdbbd
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 16:15:16 -0700

    formated code

--- src/components/About.js
+++ src/components/About.js
@@ -10,36 +10,43 @@ function urlFor(source) {
 }
 
 export default function About() {
-    const [author, setAuthor] = useState(null)
+  const [author, setAuthor] = useState(null);
 
-    useEffect(() => {
-        sanityClient.fetch(`*[_type == "author"]{
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
             name,
             bio,
             "authorImage": image.asset->url
-        }`).then((data) => setAuthor(data[0]))
-        .catch(console.error)
-    }, [])
+        }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
 
-    if (!author) return <div>Loading..</div>
+  if (!author) return <div>Loading..</div>;
 
-    return (
+  return (
     <main className="relative">
-        <img src={lights} alt="string lights" className=" absolute w-full"/>
-        <div className="p-10 lg:pt-48 container mx-auto relative">
-            <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
-                <div className="text-lg flex flex-col justify-center">
-                    <h1 className="cursive text-6xl text-green-300 mb-4">
-                        Hey. I'm{" "}
-                        <span className="text-green-100">{author.name}</span>
-                    </h1>
-                    <div className="prose lg:prose-xl text-white">
-                        <BlockContent blocks={author.bio} projectId="tqvcvwmt" />
-                    </div>
-                </div>
-            </section>
-        </div>
+      <img src={lights} alt="string lights" className=" absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            alt={author.name}
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hey. I'm <span className="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent blocks={author.bio} projectId="tqvcvwmt" />
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
-        )
+  );
 }
