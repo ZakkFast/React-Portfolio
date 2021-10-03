6d15926622816a7872c02d2cb41882722ab18f84
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 12:21:12 -0700

    set up navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowerRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
-    <BrowerRouter>
-    <switch>
-      <Route component={Home} path='/' />
-      <Route component={} path='' />
-      <Route component={} path='' />
-      <Route component={} path='' />
-      <Route component={} path='' />
-    </switch>
-    </BrowerRouter>
+    <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route component={Home} path='/' exact/>
+      <Route component={About} path='/about' />
+      <Route component={SinglePost} path='/post/:slug' />
+      <Route component={Post} path='/post' />
+      <Route component={Project} path='/project' />
+    </Switch>
+    </BrowserRouter>
   )
 }
 
