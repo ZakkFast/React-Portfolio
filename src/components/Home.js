f50ae226129537eda20b0812d4181e59dfaeaf48
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 20:41:28 -0700

    added div divider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import SanityClient from "../client.js";
+import image from "../mac.jpg"
 
 export default function Home() {
   const [clientData, setClient] = useState(null);
@@ -16,23 +17,28 @@ export default function Home() {
   });
 
   return (
-    <main className="bg-gray-300">
+    <main className="bg-white">
       <section className="grid grid-col-1 relative flex justify-center pt-12 lg:pt-64 px-8 mb-48">
         <div className="mr-64 ml-64">
-          <h1 className="text-6xl text-center font-bold text-black cursive leading-none lg:leading-snug home-name pb-16">
+          <h1 className="text-6xl font-bold text-black cursive leading-none lg:leading-snug home-name pb-16">
             Hey there. I'm{" "}
-            <span className="text-blue-600 underline">Zakk.</span>
+            <span className="text-black underline animate-pulse">Zakk.</span>
           </h1>
           <p className="text-2xl text-center text-black cursive pb-3">
             ..and I'm a Web Developer that stands out.
           </p>
         </div>
       </section>
+
+      <div className="flex justify-center">
+        <div className="bg-black w-11/12 h-0.5 "></div>
+      </div>
+
       <section className="bg-white p-12">
         <h1 className="text-5xl flex justify-center nav-font mb-12">
           Who I Build For
         </h1>
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {clientData && clientData.map((client, index) => (
           <article>
             <h3 className="text-gray-800 text-xl font-bold mb-2">
