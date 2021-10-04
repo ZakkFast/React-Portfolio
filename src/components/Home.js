8cc70375f92df8b40d6ed318936c5349e36f87d1
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 20:12:01 -0700

    added new section to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SanityClient from "../client.js";
 
 export default function Home() {
+  const [clientData, setClient] = useState(null);
+
+  useEffect(() => {
+    SanityClient.fetch(
+      `*[_type == "client"]{
+      client,
+      description
+    }`
+    )
+      .then((data) => setClient(data))
+      .catch(console.error);
+  });
+
   return (
-    <main className="bg-white">
-      <section className="grid grid-col-1 relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+    <main className="bg-gray-300">
+      <section className="grid grid-col-1 relative flex justify-center pt-12 lg:pt-64 px-8 mb-48">
         <div className="mr-64 ml-64">
-        <h1 className="text-6xl font-bold text-black cursive leading-none lg:leading-snug home-name pb-16">
-          Hey there. I'm <span className="text-blue-600 underline">Zakk.</span>
+          <h1 className="text-6xl text-center font-bold text-black cursive leading-none lg:leading-snug home-name pb-16">
+            Hey there. I'm{" "}
+            <span className="text-blue-600 underline">Zakk.</span>
+          </h1>
+          <p className="text-2xl text-center text-black cursive pb-3">
+            ..and I'm a Web Developer that stands out.
+          </p>
+        </div>
+      </section>
+      <section className="bg-white p-12">
+        <h1 className="text-5xl flex justify-center nav-font mb-12">
+          Who I Build For
         </h1>
-        <p className="text-2xl text-black cursive pb-3">
-          ..and I'm a Web Developer that stands out. 
-        </p>
+        <div className="grid grid-cols-2 gap-8">
+          {clientData && clientData.map((client, index) => (
+          <article>
+            <h3 className="text-gray-800 text-xl font-bold mb-2">
+              {client.client}
+              </h3>
+            <p>
+              {client.description}
+            </p>
+          </article>
+          ))}
         </div>
       </section>
     </main>
--- src/components/Project.js
+++ src/components/Project.js
@@ -30,7 +30,7 @@ export default function Project() {
           {projectData &&
             projectData.map((project, index) => (
               <article className="relative rounded-lg shadow-xl bg-white border-l-8 border-black p-16">
-                <h3 className="text-gray-800 texzt-3xl font-bold mb-2 hover:text-red-700">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
                   <a
                     href={project.link}
                     alt={project.title}
