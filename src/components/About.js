99ce7d9c452b72ec5f652160d82e84173b9f8707
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 21:16:18 -0700

    changed padding on card

--- src/components/About.js
+++ src/components/About.js
@@ -29,17 +29,17 @@ export default function About() {
   return (
     <main className="relative">
       <div className="p-10 lg:pt-48 container mx-auto relative min-h-screen ">
-        <section className="bg-gray-300 rounded-tl-lg shadow-2xl lg:flex p-20 border-l-8 border-black">
+        <section className="bg-gray-900 rounded shadow-2xl lg:flex p-16 border-l-8 border-black">
           <img
             src={urlFor(author.authorImage).url()}
             className="rounded-full w-32 h-32 lg:w-64 lg:h-64 mr-8"
             alt={author.name}
           />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-black mb-4">
-              Hey. I'm <span className="text-black">{author.name}</span>
+            <h1 className="cursive text-white text-6xl text-black mb-4">
+              Hey. I'm <span className="text-white">{author.name}</span>
             </h1>
-            <div className="prose lg:prose-xl text-gray-900">
+            <div className="prose lg:prose-xl text-white">
               <BlockContent blocks={author.bio} projectId="tqvcvwmt" />
             </div>
           </div>
