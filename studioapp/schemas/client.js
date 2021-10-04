f3e8eb3d9d646dd7f01930453cce5baa8b223230
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 19:43:16 -0700

    created "client" model for Sanity"

---
+++ studioapp/schemas/client.js
@@ -0,0 +1,15 @@
+export default {
+  name: "client",
+  title: "Client",
+  type: "document",
+  fields: [
+    {
+      name: "client",
+      type: "string",
+    },
+    {
+      name: "description",
+      type: "text",
+    },
+  ],
+};
