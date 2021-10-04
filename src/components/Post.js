92012d5f1749c5010142a810a4ef89eea483719d
Zakk Fast <zakkfastpro@gmail.com>
2021-10-03 18:35:42 -0700

    fixed colour scheme

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,21 +24,21 @@ export default function Post() {
   }, []);
 
   return (
-    <main className="bg-green-100 min-h-screen p-12">
+    <main className="bg-white min-h-screen p-12">
       <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive">
-          Blog Posts Page
+        <h1 className="text-5xl flex justify-center cursive mb-12">
+          What I've Been Up To
         </h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+        {/* <h2 className="text-lg text-gray-600 flex justify-center mb-12">
           Welcome to my blog.
-        </h2>
+        </h2> */}
         <div className="grid md:grid-cols-auto lg:grid-cols-3 gap-8">
           {postData &&
             postData.map((post, index) => (
               <article>
                 <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                   <span
-                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-black"
                     key={index}
                   >
                     <img
@@ -47,7 +47,7 @@ export default function Post() {
                       className="w-full h-full rounded-r object-cover absolute"
                     />
                     <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                      <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                      <h3 className="text-black text-lg font-bold px-3 py-4 bg-gray-200 bg-opacity-75 rounded hover:bg-blue-600">
                         {post.title}
                       </h3>
                     </span>
